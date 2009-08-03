3fbe2679a8ae4bc634476d5e30ead98f5a72cc90
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-08-03 15:41:55 -0700

    Vty: add vty_serv_sock_family back
    Used in some configs.

--- lib/vty.c
+++ lib/vty.c
@@ -1835,7 +1835,6 @@ vty_serv_sock_addrinfo (const char *hostname, unsigned short port)
 }
 #endif /* HAVE_IPV6 && ! NRL */
 
-#if 0
 /* Make vty server socket. */
 static void
 vty_serv_sock_family (const char* addr, unsigned short port, int family)
@@ -1900,7 +1899,6 @@ vty_serv_sock_family (const char* addr, unsigned short port, int family)
   /* Add vty server event. */
   vty_event (VTY_SERV, accept_sock, NULL);
 }
-#endif
 
 #ifdef VTYSH
 /* For sockaddr_un. */
