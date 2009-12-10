59baf77482984edf43e9c699c1dddf9f0223d661
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-12-10 08:36:09 -0800

    irdp: warning cleanup
    Do some cleanup here so it compiles without warnings:
      * move all prototypes to common header file irdp.h
      * move inet_2a to file where it is used

--- zebra/irdp_packet.c
+++ zebra/irdp_packet.c
@@ -59,6 +59,7 @@
 #include "zebra/irdp.h"
 #include <netinet/ip_icmp.h>
 #include "if.h"
+#include "checksum.h"
 #include "sockunion.h"
 #include "log.h"
 #include "sockopt.h"
@@ -67,12 +68,9 @@
 /* GLOBAL VARS */
 
 int irdp_sock = -1;
-char b1[16], b2[16], b3[16], b4[16];  /* For inet_2a */
 
 extern struct zebra_t zebrad;
 extern struct thread *t_irdp_raw;
-int in_cksum (void *ptr, int nbytes);
-void process_solicit (struct interface *ifp);
 
 static void
 parse_irdp_packet(char *p, 
