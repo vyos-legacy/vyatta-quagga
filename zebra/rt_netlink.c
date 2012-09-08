8e06033ab46b3df8d6cc1125083b02cc0b7355b4
John Southworth <john.southworth@vyatta.com>
2012-09-07 17:23:14 -0700

    Bugfix 6087: Ignore IPv6 linklocal routes in zebra, let Linux manage them on its own

--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -626,9 +626,14 @@ netlink_interface_addr (struct sockaddr_nl *snl, struct nlmsghdr *h)
   if (ifa->ifa_family == AF_INET6)
     {
       if (h->nlmsg_type == RTM_NEWADDR)
-	connected_add_ipv6 (ifp, flags,
+      {
+	if (IN6_IS_ADDR_LINKLOCAL ((struct in6_addr *) addr))
+	  return 0;
+	else
+	  connected_add_ipv6 (ifp, flags,
 			    (struct in6_addr *) addr, ifa->ifa_prefixlen,
 			    (struct in6_addr *) broad, label);
+      }
       else
         connected_delete_ipv6 (ifp,
 			       (struct in6_addr *) addr, ifa->ifa_prefixlen,
